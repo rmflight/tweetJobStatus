be29d675af75c073b398b028591822f755bef265
Robert Flight <r.flight79@gmail.com>
2014-07-03 11:24:42 -0400

    added inclusion of date-time string to the tweet

--- R/rmfNotifier.R
+++ R/rmfNotifier.R
@@ -5,7 +5,12 @@
 #' @param tweetText the text to include in the tweet
 #' @export
 #' @importFrom twitteR tweet
-jobNotify <- function(tweetText){
-  fullTweet <- paste("@rmflight", tweetText, sep=" ")
+#' @importFrom lubridate now
+jobNotify <- function(tweetText, addnow=TRUE){
+  t <- ""
+  if (addnow){
+    t <- now()
+  }
+  fullTweet <- paste("@rmflight", tweetText,  t, sep=" ")
   tweet(fullTweet)
 }
