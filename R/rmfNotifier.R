dde95b91150ba2aafec2db0d3e96029a68e02ac3
Robert M Flight <r.flight79@gmail.com>
2015-09-17 11:59:34 -0400

    add constructTweet function enabling checking tweet length and pasting pieces together

--- R/rmfNotifier.R
+++ R/rmfNotifier.R
@@ -13,4 +13,33 @@ jobNotify <- function(tweetText, addnow=TRUE){
   }
   fullTweet <- paste("@rmflight", tweetText,  t, sep=" ")
   tweet(fullTweet)
+#' constructs and checks tweets
+#' 
+#' given a userID, jobID, status text and datestring, constructs a tweet where the tweet
+#' length is <= 140 characters, trimming the jobID as necessary to make everything fit.
+#' 
+#' @param userID the userID to tweet to
+#' @param jobID text string identifying the job
+#' @param jobStatus the job status
+#' @param dateString the date string to add to the tweet
+#' @param charAllow the number of characters allowed in a tweet (currently 140)
+#' 
+#' @return character string
+constructTweet <- function(userID, jobID, jobStatus, dateString, charAllow = 140){
+  nUser <- nchar(userID)
+  nJob <- nchar(jobID)
+  nStatus <- nchar(jobStatus)
+  nDate <- nchar(dateString)
+  
+  totalChar <- nchar(userID) + nchar(jobID) + nchar(jobStatus) + nchar(dateString) + 3
+  
+  ellipsChar <- 5
+  
+  if (totalChar >= charAllow){
+    nJobAllow <- charAllow - ellipsChar - nUser - nStatus - nDate - 5
+    
+    jobID <- paste(substring(jobID, 1, nJobAllow), " ...", sep = "")
+  }
+  
+  return(paste(userID, jobID, jobStatus, dateString, sep = " "))
 }
